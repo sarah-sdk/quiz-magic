5f8346dc14c30064551d83554070d4457b06e283
Sarah Smandack <sarah.smandack@gmail.com>
2025-03-31 17:17:44 +0200

    create data.json and check if fetch ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+type QuestionType = {
+  id: number;
+  question: string;
+  answers: string;
+};
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [questions, setQuestions] = useState<QuestionType[] | null>(null);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data.questions))
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (!questions) return <div>Chargement...</div>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Quiz Magic</h1>
+      <p>Quel deck es-tu ?</p>
+      {questions.map((question) => (
+        <p key={question.id}>{question.question}</p>
+      ))}
+    </>
+  );
 }
 
 export default App;
